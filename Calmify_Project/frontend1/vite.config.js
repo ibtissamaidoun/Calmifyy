90d59768c012a1596fdcbbf0f9e12a130f02d448
MeryemeBoussaid <boussaid.meryeme@etu.uae.ac.ma>
2024-12-12 16:06:09 +0100

    first

---
+++ Calmify_Project/frontend1/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: './',  // Définit la racine du projet
+  build: {
+    outDir: 'build', // Dossier de sortie pour la build
+  },
+})
